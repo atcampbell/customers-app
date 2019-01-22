cd8c5b002ad5ea8e928bded10874217245c6575a
atcampbell <adamtoddcampbell@gmail.com>
2019-01-22 23:11:20 +0000

    tidy app

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -4,9 +4,6 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import CustomersContainer from './CustomersContainer';
 import CustomerContainer from './CustomerContainer';
 import Header from './Header';
-// import { data } from '../../../data/customers-sample';
-
-// TODO to functional component
 class App extends Component {
     render() {
         return (
@@ -15,21 +12,14 @@ class App extends Component {
                 <Router>
                     <div>
                         <Header />
-                        {/* TODO - DONT NEED THESE PROPS ANYMORE DO THE SIMPLE WAY */}
-                        {/* <Route path="/" Component={Header} /> */}
                         <Switch>
-                            {/* <Route exact path="/" render={props => <Customers {...props} />} /> */}
-                            {/* Put this back when not using local json */}
                             <Route exact path="/" component={CustomersContainer} />
-                            {/* TODO get id in here */}
                             <Route
                                 exact
                                 path="/create"
                                 render={props => <CustomerContainer {...props} create={true} />}
                             />
                             <Route exact path="/:customerID" component={CustomerContainer} />
-                            {/* <Route exact path="/:customerID" render={props => <Customer {...props} />} /> */}
-                            {/* <Route exact path="/:customerID" component={User} /> */}
                         </Switch>
                     </div>
                 </Router>
